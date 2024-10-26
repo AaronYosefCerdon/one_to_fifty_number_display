80f470fe4ad21a7eb31862259a22a58e1a0d989e
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-27 07:34:22 +0800

    Initial Pseudocode

---
+++ input_one_to_fifty_display.py
@@ -0,0 +1,7 @@
+#Ask the user to input a number from 1 to 50.
+
+#Define the range of the numbers into 5 different intervals.
+
+#Ask the user to keep inputting numbers by starting a loop and stop when an invalid input happens.
+
+#Display all of the input numbers in 5 different ranges.
