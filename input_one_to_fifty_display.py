409ecc0e8f48525a44848265eb9c8b8e17f01513
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-30 00:04:56 +0800

    Fixing alignment and breaking the loop

--- input_one_to_fifty_display.py
+++ input_one_to_fifty_display.py
@@ -10,26 +10,27 @@ number_counter = {
 while True:
          
 #Ask the user to input numbers from 1 to 50.
-enter_number = int(input("Please enter a number from 1 to 50: "))
+         enter_number = int(input("Please enter a number from 1 to 50: "))
 
 #Use an if statement to give a condition only numbers 1 to 50 are accepted.
-if enter_number >= 1 and enter_number <= 50:  
-        	
+         if 1 <= enter_number <= 50:
+            
 #Define each counter into 5 different ranges where each input will fall in.
-      if enter_number <= 10 and enter_number >=1:
-      	number_counter ["1-10"] += 1
-      elif enter_number > 10 and enter_number <=20:
-      	number_counter ["11-20"] += 1
-      elif enter_number > 20 and enter_number <=30:
-      	number_counter ["21-30"] += 1
-      elif enter_number > 30 and enter_number <=40:
-      	number_counter ["31-40"] += 1
-      elif enter_number > 40 and enter_number <=50:
-      	number_counter ["41-50"] += 1
-
-#Use an else statement to print an error message when an input is invalid and break the loop.
-else:
+            if enter_number <= 10 and enter_number >=1:
+             	 number_counter ["1-10"] += 1
+            elif enter_number <=20 and enter_number > 10:
+             	 number_counter ["11-20"] += 1
+            elif enter_number <=30 and enter_number > 20:
+              	number_counter ["21-30"] += 1
+            elif enter_number <=40 and enter_number > 30:
+              	number_counter ["31-40"] += 1
+            elif enter_number <=50 and enter_number > 40:
+              	number_counter ["41-50"] += 1 
+#Use an else statement to print an error message when an input is outside the given range.           
+         else:
             print("Your input is outside the given range.") 
+#Break the loop.
+            break  
          
 #Print the counts for each interval defined.
 print(number_counter)
