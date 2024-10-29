03b4d20184f25e07b48f00915424e6989c21210c
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-29 13:14:28 +0800

    Specific Steps Pseudocode

--- input_one_to_fifty_display.py
+++ input_one_to_fifty_display.py
@@ -1,7 +1,13 @@
-#Ask the user to input a number from 1 to 50.
+#Initialize a list to serve as a counter for each given range.
 
-#Define the range of the numbers into 5 different intervals.
+#Start a loop that will ask the user to input numbers.
 
-#Ask the user to keep inputting numbers by starting a loop and stop when an invalid input happens.
+#Ask the user to input numbers from 1 to 50.
 
-#Display all of the input numbers in 5 different ranges.
+#Use an if statement to give a condition only numbers 1 to 50 are accepted.
+
+#Define each counter into 5 different ranges where each input will fall in.
+
+#Use an else statement to print an error message when an input is invalid and break the loop.
+
+#Print the counts for each interval defined.
