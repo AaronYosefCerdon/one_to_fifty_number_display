c4dcdaece461f3f1ba8b9a970d484ed5b557e66d
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-29 23:10:21 +0800

    Adding if and elif statements and testing

--- input_one_to_fifty_display.py
+++ input_one_to_fifty_display.py
@@ -11,8 +11,19 @@ number_counter = {
 #Ask the user to input numbers from 1 to 50.
 enter_number = int(input("Please enter a number from 1 to 50: "))
 #Use an if statement to give a condition only numbers 1 to 50 are accepted.
-
+if enter_number >= 1 and enter_number <= 50:  
+        	
 #Define each counter into 5 different ranges where each input will fall in.
+      if enter_number <= 10 and enter_number >=1:
+      	number_counter ["1-10"] += 1
+      elif enter_number > 10 and enter_number <=20:
+      	number_counter ["11-20"] += 1
+      elif enter_number > 20 and enter_number <=30:
+      	number_counter ["21-30"] += 1
+      elif enter_number > 30 and enter_number <=40:
+      	number_counter ["31-40"] += 1
+      elif enter_number > 40 and enter_number <=50:
+      	number_counter ["41-50"] += 1
 
 #Use an else statement to print an error message when an input is invalid and break the loop.
 
