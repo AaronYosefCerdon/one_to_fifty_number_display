35ecdb9830f8fc5f1b12f984d0422f5276e20588
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-29 23:00:20 +0800

    Inputting a number completed

--- input_one_to_fifty_display.py
+++ input_one_to_fifty_display.py
@@ -9,7 +9,7 @@ number_counter = {
 #Start a loop that will ask the user to input numbers.
 
 #Ask the user to input numbers from 1 to 50.
-
+enter_number = int(input("Please enter a number from 1 to 50: "))
 #Use an if statement to give a condition only numbers 1 to 50 are accepted.
 
 #Define each counter into 5 different ranges where each input will fall in.
