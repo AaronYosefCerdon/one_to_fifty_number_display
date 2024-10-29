56a9bf25b20e97b7c9047f708996c1f5cf92d208
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-29 22:17:40 +0800

    Array Changed To Dictionary and Working Display Test

--- input_one_to_fifty_display.py
+++ input_one_to_fifty_display.py
@@ -1,5 +1,11 @@
-#Initialize a list to serve as a counter for each given range.
-
+#Initialize a dictionary to serve as a counter for each given range.
+number_counter = {
+         "1-10": 0,
+         "11-20": 0,
+         "21-30": 0,
+         "31-40": 0,
+         "41-50": 0         
+}
 #Start a loop that will ask the user to input numbers.
 
 #Ask the user to input numbers from 1 to 50.
@@ -11,3 +17,4 @@
 #Use an else statement to print an error message when an input is invalid and break the loop.
 
 #Print the counts for each interval defined.
+print(number_counter)
